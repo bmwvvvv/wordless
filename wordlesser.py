675348b8583cb2648cf4ed4d31966e65492d3033
Ma Chao <814640709@qq.com>
2019-07-11 23:30:54 +0800

    add words database

--- wordlesser.py
+++ wordlesser.py
@@ -1,17 +1,29 @@
 import sys
 import random
+import openpyxl as xls
 from PySide2 import QtCore,QtWidgets,QtGui
 
+class Words:
+    def __init__(self, path):
+        self.wb = xls.load_workbook(path)
+        self.ws = self.wb.active
 
+    def getCol(self, col):
+        return list(map(lambda cell: cell.value, self.ws[col]))
+        
+        
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
+        self.words = Words('words.xlsx') 
 
         self.hello = ["aaa", "bbb", "ccc", "ddd"]
 
         self.button = QtWidgets.QPushButton("Click me!")
+
         self.text = QtWidgets.QLabel("Hello World")
         self.text.setAlignment(QtCore.Qt.AlignCenter)
+        self.text.setWordWrap(True)
 
         self.chinese = QtWidgets.QLineEdit("guess what")
 
@@ -24,7 +36,7 @@ class MyWidget(QtWidgets.QWidget):
         self.button.clicked.connect(self.magic)
 
     def magic(self):
-        self.text.setText(random.choice(self.hello))
+        self.text.setText('<font face="verdana" color="maroon" size="60"><b>'+random.choice(self.words.getCol('A'))+'</b></font>')
 
     
 if __name__ == "__main__":
