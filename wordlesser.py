eb2a698bb6e17fe3154770597cae3ea51f3fb5df
Ma Chao <814640709@qq.com>
2019-07-12 23:03:58 +0800

    create Class Word

--- wordlesser.py
+++ wordlesser.py
@@ -3,19 +3,42 @@ import random
 import openpyxl as xls
 from PySide2 import QtCore,QtWidgets,QtGui
 
-class Words:
+
+class Word:
+    def __init__(self, index, liter, partOfSpeech, chinese, occurs, shots, flag):
+        self.index   = index
+        self.liter   = liter
+        self.pospch  = partOfSpeech
+        self.chinese = chinese
+        self.occurs  = occurs
+        self.shots   = shots
+        self.flag    = flag
+
+class Lexicon:
     def __init__(self, path):
         self.wb = xls.load_workbook(path)
         self.ws = self.wb.active
+        self.totalWordNum = self.ws.max_row
 
-    def getCol(self, col):
-        return list(map(lambda cell: cell.value, self.ws[col]))
+    def getOneWord(self, row = 1):
+        if row is 0:
+            wordAttrs = list(self.ws.rows)[0]
+        else:
+            wordAttrs = list(self.ws.rows)[row - 1]
+
+        return Word(row,
+                    wordAttrs[0].value,
+                    wordAttrs[1].value,
+                    wordAttrs[2].value,
+                    wordAttrs[3].value,
+                    wordAttrs[4].value,
+                    wordAttrs[5].value)
         
         
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-        self.words = Words('words.xlsx') 
+        self.lex = Lexicon('words.xlsx') 
 
         self.hello = ["aaa", "bbb", "ccc", "ddd"]
 
@@ -36,7 +59,8 @@ class MyWidget(QtWidgets.QWidget):
         self.button.clicked.connect(self.magic)
 
     def magic(self):
-        self.text.setText('<font face="verdana" color="maroon" size="60"><b>'+random.choice(self.words.getCol('A'))+'</b></font>')
+        index = random.randint(1, self.lex.totalWordNum)    
+        self.text.setText('<font face="verdana" color="maroon" size="60"><b>'+self.lex.getOneWord(index).liter+'</b></font>')
 
     
 if __name__ == "__main__":
