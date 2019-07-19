f2202bc36f89090e298b8dc07c18d6221794031f
Rick <814640709@qq.com>
2019-07-20 00:07:35 +0800

    conform to PEP 8

--- wordlesser.py
+++ wordlesser.py
@@ -2,18 +2,19 @@ import re
 import sys
 import random
 import openpyxl as xls
-from PySide2 import QtCore,QtWidgets,QtGui
+from PySide2 import QtCore, QtWidgets, QtGui
 
 
 class Word:
-    def __init__(self, index, liter, partOfSpeech, chinese, occurs, shots, flag):
-        self.index   = index
-        self.liter   = liter
-        self.pospch  = partOfSpeech
+    def __init__(self, index, liter, part_of_speech, chinese, occurs, shots, flag):
+        self.index = index
+        self.liter = liter
+        self.pos = part_of_speech
         self.chinese = chinese
-        self.occurs  = occurs
-        self.shots   = shots
-        self.flag    = flag
+        self.occurs = occurs
+        self.shots = shots
+        self.flag = flag
+
 
 class Lexicon:
     def __init__(self, path):
@@ -22,47 +23,48 @@ class Lexicon:
         self.ws = self.wb.active
         self.totalWordNum = self.ws.max_row
 
-    def updateOccursOfWord(self, word):
-       self.ws.cell(column=4, row=word.index, value=word.occurs)
+    def update_occurs_of_word(self, word):
+        self.ws.cell(column=4, row=word.index, value=word.occurs)
 
-    def updateShotsOfWord(self, word):
-       self.ws.cell(column=5, row=word.index, value=word.shots)
+    def update_shots_of_word(self, word):
+        self.ws.cell(column=5, row=word.index, value=word.shots)
 
-    def getOneWord(self, row = 1):
+    def get_one_word(self, row=1):
         if row is 0:
             wordAttrs = list(self.ws.rows)[0]
         else:
             wordAttrs = list(self.ws.rows)[row - 1]
 
         word = Word(row,
-                    wordAttrs[0].value,     # literal
-                    wordAttrs[1].value,     # part of speech
-                    wordAttrs[2].value,     # Chinese
-                    wordAttrs[3].value,     # occurs
-                    wordAttrs[4].value,     # shots
-                    wordAttrs[5].value)     # flag
+                    wordAttrs[0].value,  # literal
+                    wordAttrs[1].value,  # part of speech
+                    wordAttrs[2].value,  # Chinese
+                    wordAttrs[3].value,  # occurs
+                    wordAttrs[4].value,  # shots
+                    wordAttrs[5].value)  # flag
 
         word.occurs += 1
-        self.updateOccursOfWord(word)    # guarantee the consistency of data
+        self.update_occurs_of_word(word)  # guarantee the consistency of data
 
         return word
-        
+
+
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-        self.lex = Lexicon('words.xlsx') 
+        self.lex = Lexicon('words.xlsx')
         self.wordInd = random.randint(1, self.lex.totalWordNum)
-        self.word = self.lex.getOneWord(self.wordInd)
+        self.word = self.lex.get_one_word(self.wordInd)
 
         self.initUI()
 
     def initUI(self):
         self.button = QtWidgets.QPushButton("Click me!")
 
-        self.curWord = QtWidgets.QLabel('<font face="verdana" color="maroon" size="10"><b>'+self.word.liter+'</b></font>')
+        self.curWord = QtWidgets.QLabel(
+            '<font face="verdana" color="maroon" size="10"><b>' + self.word.liter + '</b></font>')
         self.curWord.setAlignment(QtCore.Qt.AlignCenter)
         self.curWord.setScaledContents(True)
-
 
         self.prevWord = QtWidgets.QLabel(self.word.liter + "\n" + self.word.chinese)
         self.prevWord.setAlignment(QtCore.Qt.AlignCenter)
@@ -80,17 +82,16 @@ class MyWidget(QtWidgets.QWidget):
         self.hintImage.setAlignment(QtCore.Qt.AlignCenter)
         self.hintImage.setPixmap(self.negImage)
 
-
         self.chinese = QtWidgets.QLineEdit()
         self.chinese.returnPressed.connect(self.magic)
 
         self.layout = QtWidgets.QGridLayout()
         self.layout.setSpacing(5)
         self.layout.addWidget(self.prevWord, 0, 0, 1, 1)
-        self.layout.addWidget(self.curWord,  1, 1, 1, 1)
-        self.layout.addWidget(self.hintImage,2, 1, 1, 1)
-        self.layout.addWidget(self.chinese,  3, 1, 1, 1)
-        self.layout.addWidget(self.button,   4, 1, 1, 1)
+        self.layout.addWidget(self.curWord, 1, 1, 1, 1)
+        self.layout.addWidget(self.hintImage, 2, 1, 1, 1)
+        self.layout.addWidget(self.chinese, 3, 1, 1, 1)
+        self.layout.addWidget(self.button, 4, 1, 1, 1)
         self.layout.setColumnStretch(0, 1)
         self.layout.setColumnStretch(1, 2)
         self.layout.setColumnStretch(2, 1)
@@ -98,27 +99,28 @@ class MyWidget(QtWidgets.QWidget):
         self.layout.setRowStretch(1, 2)
         self.layout.setRowStretch(2, 1)
         self.layout.setRowStretch(3, 1)
-        
+
         self.setLayout(self.layout)
 
         self.button.clicked.connect(self.magic)
 
     def refreshPrevWord(self):
-        self.prevWord.setText("<font face=verdana size=20><b>"+self.word.liter+"<br /></b></font>"
-                             +"<font face=verdana size=20><b>"+self.word.chinese+"</b></font>")
+        self.prevWord.setText("<font face=verdana size=20><b>" + self.word.liter + "<br /></b></font>"
+                              + "<font face=verdana size=20><b>" + self.word.chinese + "</b></font>")
 
     def retrieveWord(self):
-        self.wordInd = random.randint(1, self.lex.totalWordNum)    
-        self.word = self.lex.getOneWord(self.wordInd)
-        if round(pow(2, self.word.shots/self.word.occurs)) is 2:
-            tmpWordInd = random.randint(1, self.lex.totalWordNum)    
+        self.wordInd = random.randint(1, self.lex.totalWordNum)
+        self.word = self.lex.get_one_word(self.wordInd)
+        if round(pow(2, self.word.shots / self.word.occurs)) is 2:
+            tmpWordInd = random.randint(1, self.lex.totalWordNum)
             if self.wordInd == tmpWordInd:
                 pass
             else:
-                self.wordInd = random.randint(1, self.lex.totalWordNum)    
-                self.word = self.lex.getOneWord(self.wordInd)
+                self.wordInd = random.randint(1, self.lex.totalWordNum)
+                self.word = self.lex.get_one_word(self.wordInd)
 
-        self.curWord.setText('<h6><font face="verdana" color="maroon" size="10"><b>'+self.word.liter+'</b></font></h6>')
+        self.curWord.setText(
+            '<h6><font face="verdana" color="maroon" size="10"><b>' + self.word.liter + '</b></font></h6>')
         self.hintImage.setPixmap(self.negImage)
         self.hintImage.update()
 
@@ -128,15 +130,15 @@ class MyWidget(QtWidgets.QWidget):
         chineseStr = self.chinese.text()
         if chineseStr == '':
             self.word.occurs -= 1
-            self.lex.updateOccursOfWord(self.word)
+            self.lex.update_occurs_of_word(self.word)
             return
 
         if re.search(chineseStr, self.word.chinese, re.X) is not None:
             self.word.shots += 1
-            self.lex.updateShotsOfWord(self.word)
+            self.lex.update_shots_of_word(self.word)
             self.hintImage.setPixmap(self.posImage)
             self.hintImage.update()
-            
+
         self.chinese.clear()
 
     def magic(self):
@@ -144,12 +146,12 @@ class MyWidget(QtWidgets.QWidget):
         self.lex.wb.save(self.lex.filename)
         self.retrieveWord()
 
-    
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
 
     widget = MyWidget()
     widget.resize(800, 600)
     widget.show()
-    
+
     sys.exit(app.exec_())
